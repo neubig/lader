5a94bc3f344f959851f707a3635719838e4863a1
Graham Neubig <neubig@gmail.com>
2012-01-24 14:11:37 +0900

    Added missing cc files

---
+++ src/lib/hyper-graph.cc
@@ -0,0 +1,81 @@
+#include <kyldr/hyper-graph.h>
+
+using namespace kyldr;
+using namespace std;
+
+HyperNode * HyperGraph::GetNodeAtSpan(const HyperSpan & span,
+                                      bool add) {
+    SpanNodeMap::iterator it = span_node_map_.find(span);
+    // If the node is found, return it right away
+    if(it != span_node_map_.end())
+        return it->second;
+    if(!add)
+        return NULL;
+    // If the node is not found, add it
+    nodes_.push_back(new HyperNode(nodes_.size(), span));
+    HyperNode * ret = *nodes_.rbegin();
+    // Add to the map
+    span_node_map_.insert(MakePair(span, ret));
+    return ret;
+}
+
+void HyperGraph::AddTerminals(const CombinedAlignment & combined,
+                              int max_len,
+                              bool use_inverse) {
+    int len = combined.size();
+    for(int i = 0; i < len; i++) {
+        int end = min(len, i+max_len);
+        for(int j = i; j < end; j++) {
+            // Add the straight terminal
+            HyperNode * str_node = 
+                GetNodeAtSpan(HyperSpan(i,j,combined[i],combined[j]));
+            HyperEdge * str_edge = AddNewEdge(HyperEdge::EDGE_TERMSTR);
+            str_node->AddEdge(str_edge);
+            // Add the inverse terminal
+            if(use_inverse) {
+                HyperNode * inv_node = 
+                    GetNodeAtSpan(HyperSpan(i,j,combined[j],combined[i]));
+                HyperEdge * inv_edge = AddNewEdge(HyperEdge::EDGE_TERMINV);
+                inv_node->AddEdge(inv_edge);
+            }
+        }
+    }
+}
+
+void HyperGraph::AddNonTerminalPair(const HyperNode * left, 
+                                    const HyperNode * right) {
+    const HyperSpan & l = left->GetSpan(), & r = right->GetSpan();
+    // Combine the straight non-terminal
+    HyperNode * str_node = GetNodeAtSpan(
+        HyperSpan(l.GetLeft(), r.GetRight(), l.GetTrgLeft(), r.GetTrgRight()));
+    str_node->AddEdge(AddNewEdge(HyperEdge::EDGE_STR));
+    // Combine the inverted non-terminal (note l and r are switched)
+    HyperNode * inv_node = GetNodeAtSpan(
+        HyperSpan(l.GetLeft(), r.GetRight(), r.GetTrgLeft(), l.GetTrgRight()));
+    inv_node->AddEdge(AddNewEdge(HyperEdge::EDGE_INV));
+}
+
+void HyperGraph::AddNonTerminals() {
+    // Go through all the nodes
+    for(int i = 0; i < (int)nodes_.size(); i++) {
+        // Save the current values
+        HyperNode * node = nodes_[i];
+        // Travel through and combine all left neighbors
+        const std::vector<HyperNode*> & left_neighbors
+            = GetLeftNeighbors(node->GetSpan().GetLeft());
+        for(int j = 0; j < (int)left_neighbors.size(); j++)
+            AddNonTerminalPair(left_neighbors[j], node);
+        // Travel through and combine all right neighbors
+        const std::vector<HyperNode*> & right_neighbors
+            = GetRightNeighbors(node->GetSpan().GetRight());
+        for(int j = 0; j < (int)right_neighbors.size(); j++)
+            AddNonTerminalPair(node, right_neighbors[j]);
+        // Add the current span to the neighboring nodes
+        int r = node->GetSpan().GetRight() + 1, 
+            l = node->GetSpan().GetLeft() - 1;
+        if(r != (int)left_neighbors_.size())
+            SafeAccess(left_neighbors_, r).push_back(node);
+        if(l != -1)
+            SafeAccess(right_neighbors_, l).push_back(node);
+    }
+}
---
+++ src/test/test-kyldr.cc
@@ -0,0 +1,27 @@
+#include "test-combined-alignment.h"
+#include "test-hyper-graph.h"
+#include <vector>
+
+using namespace std;
+using namespace kyldr;
+
+int main() {
+    // Initialize all the tests
+    vector<TestBase*> tests;
+    tests.push_back(new TestCombinedAlignment());
+    tests.push_back(new TestHyperGraph());
+    // Run all the tests
+    int number_passed = 0;
+    for(int i = 0; i < (int)tests.size(); i++)
+        if(tests[i]->RunTest())
+            number_passed++;
+    // Check whether all were passed or not
+    if(number_passed == (int)tests.size()) {
+        cout << "**** passed ****" << endl;
+    } else {
+        cout << "**** FAILED!!! ****" << endl;
+    }
+    // Deallocate
+    for(int i = 0; i < (int)tests.size(); i++)
+        delete tests[i]; 
+}
